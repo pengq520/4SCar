5de46cc2101814563022532effde9665ee80667b
cxqy <179182257@qq.com>
2017-11-22 15:55:30 +0800

    维修保养控制类

---
+++ app/src/main/java/com/example/ios27/a4scar/UpkeepManage.java
@@ -0,0 +1,15 @@
+package com.example.ios27.a4scar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UpkeepManage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //cessabdfbaksjcnjkwajnbcvkjabvc
+    }
+}
